c7614de0c4b9808ab0a4df593e93cf6d9279f5bf
MBravoCanales <76415021+MBravoCanales@users.noreply.github.com>
2023-06-18 00:28:02 -0400

    cambios

--- WinFormsApp1/Costos.Designer.vb
+++ WinFormsApp1/Costos.Designer.vb
@@ -22,24 +22,12 @@ Partial Class Costos
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Label4 = New Label()
-        Label3 = New Label()
-        Label2 = New Label()
-        Label1 = New Label()
-        GroupBox1 = New GroupBox()
-        txtIngrediente1 = New TextBox()
-        txtCantidad1 = New TextBox()
-        txtCoste1 = New TextBox()
-        txtValor1 = New TextBox()
-        Button1 = New Button()
-        Button5 = New Button()
         Button2 = New Button()
         Button4 = New Button()
         Label5 = New Label()
         TabControl1 = New TabControl()
         TabPage1 = New TabPage()
         TabPage2 = New TabPage()
-        TabPage3 = New TabPage()
         GroupBox2 = New GroupBox()
         TextBox1 = New TextBox()
         TextBox2 = New TextBox()
@@ -51,6 +39,7 @@ Partial Class Costos
         Button6 = New Button()
         Label8 = New Label()
         Label9 = New Label()
+        TabPage3 = New TabPage()
         GroupBox3 = New GroupBox()
         TextBox5 = New TextBox()
         TextBox6 = New TextBox()
@@ -62,109 +51,12 @@ Partial Class Costos
         Button8 = New Button()
         Label12 = New Label()
         Label13 = New Label()
-        GroupBox1.SuspendLayout()
         TabControl1.SuspendLayout()
-        TabPage1.SuspendLayout()
         TabPage2.SuspendLayout()
-        TabPage3.SuspendLayout()
         GroupBox2.SuspendLayout()
+        TabPage3.SuspendLayout()
         GroupBox3.SuspendLayout()
         SuspendLayout()
-        ' 
-        ' Label4
-        ' 
-        Label4.AutoSize = True
-        Label4.Location = New Point(575, 24)
-        Label4.Name = "Label4"
-        Label4.Size = New Size(116, 15)
-        Label4.TabIndex = 3
-        Label4.Text = "Valor (unidad/gr/ml)"
-        ' 
-        ' Label3
-        ' 
-        Label3.AutoSize = True
-        Label3.Location = New Point(452, 24)
-        Label3.Name = "Label3"
-        Label3.Size = New Size(97, 15)
-        Label3.TabIndex = 2
-        Label3.Text = "Coste de compra"
-        ' 
-        ' Label2
-        ' 
-        Label2.AutoSize = True
-        Label2.Location = New Point(247, 24)
-        Label2.Name = "Label2"
-        Label2.Size = New Size(183, 15)
-        Label2.TabIndex = 1
-        Label2.Text = "Cantidad (unidad/gramo/militro)"
-        ' 
-        ' Label1
-        ' 
-        Label1.AutoSize = True
-        Label1.Location = New Point(42, 24)
-        Label1.Name = "Label1"
-        Label1.Size = New Size(67, 15)
-        Label1.TabIndex = 0
-        Label1.Text = "Ingrediente"
-        ' 
-        ' GroupBox1
-        ' 
-        GroupBox1.Controls.Add(txtIngrediente1)
-        GroupBox1.Controls.Add(txtCantidad1)
-        GroupBox1.Controls.Add(txtCoste1)
-        GroupBox1.Controls.Add(txtValor1)
-        GroupBox1.Location = New Point(42, 42)
-        GroupBox1.Name = "GroupBox1"
-        GroupBox1.Size = New Size(661, 45)
-        GroupBox1.TabIndex = 9
-        GroupBox1.TabStop = False
-        ' 
-        ' txtIngrediente1
-        ' 
-        txtIngrediente1.Location = New Point(6, 15)
-        txtIngrediente1.Name = "txtIngrediente1"
-        txtIngrediente1.Size = New Size(186, 23)
-        txtIngrediente1.TabIndex = 4
-        ' 
-        ' txtCantidad1
-        ' 
-        txtCantidad1.Location = New Point(209, 15)
-        txtCantidad1.Name = "txtCantidad1"
-        txtCantidad1.Size = New Size(183, 23)
-        txtCantidad1.TabIndex = 5
-        ' 
-        ' txtCoste1
-        ' 
-        txtCoste1.Location = New Point(410, 15)
-        txtCoste1.Name = "txtCoste1"
-        txtCoste1.Size = New Size(100, 23)
-        txtCoste1.TabIndex = 6
-        ' 
-        ' txtValor1
-        ' 
-        txtValor1.Enabled = False
-        txtValor1.Location = New Point(539, 15)
-        txtValor1.Name = "txtValor1"
-        txtValor1.Size = New Size(116, 23)
-        txtValor1.TabIndex = 7
-        ' 
-        ' Button1
-        ' 
-        Button1.Location = New Point(13, 57)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(23, 23)
-        Button1.TabIndex = 8
-        Button1.Text = "-"
-        Button1.UseVisualStyleBackColor = True
-        ' 
-        ' Button5
-        ' 
-        Button5.Location = New Point(709, 57)
-        Button5.Name = "Button5"
-        Button5.Size = New Size(23, 23)
-        Button5.TabIndex = 6
-        Button5.Text = "+"
-        Button5.UseVisualStyleBackColor = True
         ' 
         ' Button2
         ' 
@@ -207,13 +99,6 @@ Partial Class Costos
         ' 
         ' TabPage1
         ' 
-        TabPage1.Controls.Add(GroupBox1)
-        TabPage1.Controls.Add(Label1)
-        TabPage1.Controls.Add(Button1)
-        TabPage1.Controls.Add(Label2)
-        TabPage1.Controls.Add(Button5)
-        TabPage1.Controls.Add(Label4)
-        TabPage1.Controls.Add(Label3)
         TabPage1.Location = New Point(4, 24)
         TabPage1.Name = "TabPage1"
         TabPage1.Padding = New Padding(3)
@@ -234,27 +119,10 @@ Partial Class Costos
         TabPage2.Location = New Point(4, 24)
         TabPage2.Name = "TabPage2"
         TabPage2.Padding = New Padding(3)
-        TabPage2.Size = New Size(772, 296)
+        TabPage2.Size = New Size(818, 296)
         TabPage2.TabIndex = 1
         TabPage2.Text = "Agregar"
         TabPage2.UseVisualStyleBackColor = True
-        ' 
-        ' TabPage3
-        ' 
-        TabPage3.Controls.Add(GroupBox3)
-        TabPage3.Controls.Add(Label10)
-        TabPage3.Controls.Add(Button7)
-        TabPage3.Controls.Add(Label11)
-        TabPage3.Controls.Add(Button8)
-        TabPage3.Controls.Add(Label12)
-        TabPage3.Controls.Add(Label13)
-        TabPage3.Location = New Point(4, 24)
-        TabPage3.Name = "TabPage3"
-        TabPage3.Padding = New Padding(3)
-        TabPage3.Size = New Size(772, 296)
-        TabPage3.TabIndex = 2
-        TabPage3.Text = "Modificar"
-        TabPage3.UseVisualStyleBackColor = True
         ' 
         ' GroupBox2
         ' 
@@ -350,6 +218,23 @@ Partial Class Costos
         Label9.Size = New Size(97, 15)
         Label9.TabIndex = 12
         Label9.Text = "Coste de compra"
+        ' 
+        ' TabPage3
+        ' 
+        TabPage3.Controls.Add(GroupBox3)
+        TabPage3.Controls.Add(Label10)
+        TabPage3.Controls.Add(Button7)
+        TabPage3.Controls.Add(Label11)
+        TabPage3.Controls.Add(Button8)
+        TabPage3.Controls.Add(Label12)
+        TabPage3.Controls.Add(Label13)
+        TabPage3.Location = New Point(4, 24)
+        TabPage3.Name = "TabPage3"
+        TabPage3.Padding = New Padding(3)
+        TabPage3.Size = New Size(818, 296)
+        TabPage3.TabIndex = 2
+        TabPage3.Text = "Modificar"
+        TabPage3.UseVisualStyleBackColor = True
         ' 
         ' GroupBox3
         ' 
@@ -458,17 +343,13 @@ Partial Class Costos
         FormBorderStyle = FormBorderStyle.None
         Name = "Costos"
         Text = "Inredientes"
-        GroupBox1.ResumeLayout(False)
-        GroupBox1.PerformLayout()
         TabControl1.ResumeLayout(False)
-        TabPage1.ResumeLayout(False)
-        TabPage1.PerformLayout()
         TabPage2.ResumeLayout(False)
         TabPage2.PerformLayout()
-        TabPage3.ResumeLayout(False)
-        TabPage3.PerformLayout()
         GroupBox2.ResumeLayout(False)
         GroupBox2.PerformLayout()
+        TabPage3.ResumeLayout(False)
+        TabPage3.PerformLayout()
         GroupBox3.ResumeLayout(False)
         GroupBox3.PerformLayout()
         ResumeLayout(False)
@@ -476,18 +357,7 @@ Partial Class Costos
     End Sub
     Friend WithEvents Button2 As Button
     Friend WithEvents Button4 As Button
-    Friend WithEvents Label4 As Label
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Label1 As Label
-    Friend WithEvents txtValor1 As TextBox
-    Friend WithEvents txtCoste1 As TextBox
-    Friend WithEvents txtCantidad1 As TextBox
-    Friend WithEvents txtIngrediente1 As TextBox
     Friend WithEvents Label5 As Label
-    Friend WithEvents Button5 As Button
-    Friend WithEvents Button1 As Button
-    Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents TabControl1 As TabControl
     Friend WithEvents TabPage1 As TabPage
     Friend WithEvents TabPage2 As TabPage
--- WinFormsApp1/Costos.vb
+++ WinFormsApp1/Costos.vb
@@ -1,5 +1,4 @@
 ﻿Public Class Costos
-    Private contador As Integer = 1 ' Variable contador para el número de group boxes generados
     Private Sub Ingredientes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
@@ -15,25 +14,7 @@
 
     End Sub
 
-    Private Sub Button5_Click_1(sender As Object, e As EventArgs) Handles Button5.Click
-
-    End Sub
-
-    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles txtValor1.TextChanged
-
-
-    End Sub
-
-    Private Sub txtCoste1_TextChanged(sender As Object, e As EventArgs) Handles txtCoste1.TextChanged
-        Dim cantidad As Double = Double.Parse(txtCantidad1.Text)
-        Dim coste As Double = Double.Parse(txtCoste1.Text)
-
-        Dim valor As Double = coste / cantidad
-
-        txtValor1.Text = valor.ToString
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
 
     End Sub
 End Class
